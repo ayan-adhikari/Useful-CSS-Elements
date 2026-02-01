cb4bf48dc15cc1f0855b2cf9844f86ae4d70d019
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-02-01 08:27:41 +0800

    Fix: general bug fixes in  index.html, script.js and layout.css

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //Run when page loads
 console.log("Page loaded")
 //Scroll to top function when elements loads
+/*
 const top = document.getElementById("top")
 function scrollToTop()
 {
     top.scrollIntoView()
 }
 window.addEventListener("load", scrollToTop)
+document.querySelector("#button-1-div").addEventListener("click", buttonClick)
+function buttonClick()
+{
+    document.querySelector("#button-1-div").textContent = "Hi"
+}*/
